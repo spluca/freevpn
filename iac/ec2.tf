b941613db6a5cef77b25ecfbf3184dba96124ffd
Antonio Pardo <apardo@spluca.org>
2024-11-05 16:30:58 +0100

    fix query

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -3,7 +3,7 @@ data "aws_ami" "freevpn_ami" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+    values = ["*ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
@@ -14,7 +14,7 @@ data "aws_ami" "freevpn_ami" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "freevpn_ec2" {
   ami           = "data.freevpn_ami.ubuntu.id"
   instance_type = "t2.micro"
 
